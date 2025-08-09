675357d9883545f44b67b9d6f7f2031cc0fb05c4
Prathamesh <prathamesh19p@gmail.com>
2025-08-09 14:24:06 +0530

    test layer

---
+++ src/test/java/com/p/circuitbreaker/SpringBootCircuitbreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p.circuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCircuitbreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

2b3ad3a64f83df50341d469f1bdfc841375faf5a
Prathamesh <prathamesh19p@gmail.com>
2025-08-09 14:23:56 +0530

    main app

---
+++ src/main/java/com/p/circuitbreaker/SpringBootCircuitbreakerApplication.java
@@ -0,0 +1,13 @@
+package com.p.circuitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCircuitbreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCircuitbreakerApplication.class, args);
+	}
+
+}
